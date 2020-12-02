c91632fbefd2ddbe8ba7ad65112a451ef0312500
[Ian Preston] <ian.preston@wcb.ab.ca>
2020-12-01 21:19:44 -0700

    part 2 complete

--- src/advent/day01/main.py
+++ src/advent/day01/main.py
@@ -49,3 +49,44 @@ def part1(filename: str = "input.txt") -> int:
         else:
             high_index -= 1
     return -1
+
+
+def part2(filename: str = "input.txt") -> int:
+    """Solve part 2 of the challenge.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file, should be in the same folder as this module
+
+    Returns
+    -------
+    int
+        The product of the trio of numbers that add to 2020
+    """
+    sorted_inputs: List[int] = sorted(read_inputs(filename))
+    low_index: int = 0
+    mid_index: int = 1
+    high_index: int = len(sorted_inputs) - 1
+    while low_index < high_index:
+        mid_index = (high_index - low_index) // 2
+        low: int = sorted_inputs[low_index]
+        mid: int = sorted_inputs[mid_index]
+        high: int = sorted_inputs[high_index]
+        if low + mid + high == 2020:
+            return low * mid * high
+        elif low + mid + high < 2020:
+            while mid_index < high_index:
+                mid_index += 1
+                mid = sorted_inputs[mid_index]
+                if low + mid + high == 2020:
+                    return low * mid * high
+            low_index += 1
+        else:
+            while mid_index > low_index:
+                mid_index -= 1
+                mid = sorted_inputs[mid_index]
+                if low + mid + high == 2020:
+                    return low * mid * high
+            high_index -= 1
+    return -1
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -12,3 +12,15 @@ def test_part_1_actual():
     """Check the actual answer to part 1."""
     test_result = main.part1()
     assert test_result == 1019571
+
+
+def test_part_2_example():
+    """Check the example for part 2."""
+    test_result = main.part2("example.txt")
+    assert test_result == 241861950
+
+
+def test_part_2_actual():
+    """Check the actual for part 2."""
+    test_result = main.part2()
+    assert test_result == 100655544
