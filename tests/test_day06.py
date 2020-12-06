82c76bf8b2155d2e163125d2e4c1d3f778320e6e
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-12-06 16:20:33 +0000

    finished part2

--- tests/test_day06.py
+++ tests/test_day06.py
@@ -12,3 +12,15 @@ def test_part_1_actual():
     """Check the example for part 1."""
     test_result = main.part1()
     assert test_result == 6457
+
+
+def test_part_2_example():
+    """Check the example for part 2."""
+    test_result = main.part2("example.txt")
+    assert test_result == 6
+
+
+def test_part_2_actual():
+    """Check the example for part 2."""
+    test_result = main.part2()
+    assert test_result == 3260
