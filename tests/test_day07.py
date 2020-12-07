a0f4a70203b21f90ffe0c57b5e926ff1c29e1ea0
[Ian Preston] <ian.preston@wcb.ab.ca>
2020-12-07 15:34:28 -0700

    finished part1

--- tests/test_day07.py
+++ tests/test_day07.py
@@ -5,22 +5,22 @@ from advent.day07 import main
 def test_part_1_example():
     """Check the example for part 1."""
     test_result = main.part1("example.txt")
-    assert test_result == 11
+    assert test_result == 4
 
 
 def test_part_1_actual():
     """Check the example for part 1."""
     test_result = main.part1()
-    assert test_result == 6457
+    assert test_result == 121
 
 
-def test_part_2_example():
-    """Check the example for part 2."""
-    test_result = main.part2("example.txt")
-    assert test_result == 6
+# def test_part_2_example():
+#     """Check the example for part 2."""
+#     test_result = main.part2("example.txt")
+#     assert test_result == 126
 
 
-def test_part_2_actual():
-    """Check the example for part 2."""
-    test_result = main.part2()
-    assert test_result == 3260
+# def test_part_2_actual():
+#     """Check the example for part 2."""
+#     test_result = main.part2()
+#     assert test_result == 3260
