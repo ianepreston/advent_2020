24a66e5d1850457232f40f1b692a56e59bbd696e
[Ian Preston] <ian.preston@wcb.ab.ca>
2020-12-08 07:01:55 -0700

    finished part 1

--- src/advent/day08/main.py
+++ src/advent/day08/main.py
@@ -2,26 +2,81 @@
 from __future__ import annotations
 
 from pathlib import Path
-import re
-from typing import Dict, List, Optional, Tuple
+from typing import List, NamedTuple, Set, Union
 
 
-def read_inputs(filename: str = "input.txt") -> Dict[str, Bag]:
-    """Read in and parse a text file of inputs.
+class Instruction(NamedTuple):
+    """I'm an instruction."""
 
-    Parameters
-    ----------
-    filename: str
-        The name of the file in this directory to load
+    cmd: str
+    num: int
 
-    Returns
-    -------
-    dict:
-        A dictionary of all bags
-    """
-    here: Path = Path(__file__).resolve().parent
-    in_path: Path = here / filename
-    with open(in_path, "r") as f:
+
+class Compy:
+    """I'm a computer."""
+
+    def __init__(self) -> None:
+        """Instantiate with index and accumulator at 0. No instructions loaded."""
+        self.index: int = 0
+        self.accumulator: int = 0
+        self.instructions: List[Instruction] = []
+
+    @staticmethod
+    def _parse_instruction(line: str) -> Instruction:
+        """Turn a line of text into an Instruction.
+        
+        Parameters
+        ----------
+        line: str
+            An instruction in string form
+        
+        Returns
+        -------
+        Instruction
+            The parsed instruction
+        """
+        cmd, num = line.strip().split()
+        return Instruction(cmd, int(num))
+
+    def load_program(self, file: Union[Path, str]) -> None:
+        """Read in a text file to get a list of Instructions.
+
+        Parameters
+        ----------
+        file: Path
+            The file to load
+        """
+        with open(file, "r") as f:
+            self.instructions = [
+                self._parse_instruction(line) for line in f.readlines()
+            ]
+
+    def _exec_instruction(self, instruction: Instruction) -> None:
+        """Run an instruction.
+        
+        Parameters
+        ----------
+        instruction: Instruction
+            The instruction to run
+        """
+        if instruction.cmd == "nop":
+            pass
+            self.index += 1
+        elif instruction.cmd == "acc":
+            self.accumulator += instruction.num
+            self.index += 1
+        elif instruction.cmd == "jmp":
+            if 0 <= self.index + instruction.num < len(self.instructions):
+                self.index += instruction.num
+            else:
+                raise IndexError("Jumped out of instruction set")
+        else:
+            raise ValueError(f"Unsupported instruction: {instruction.cmd}")
+
+    def step(self) -> None:
+        """Execute the next instruction."""
+        instruction: Instruction = self.instructions[self.index]
+        self._exec_instruction(instruction)
 
 
 def part1(filename: str = "input.txt") -> int:
@@ -35,22 +90,31 @@ def part1(filename: str = "input.txt") -> int:
     Returns
     -------
     int:
-        The number of bags that contain the shiny gold bag
+        The value of the accumulator right before a loop
     """
-    pass
+    indices: Set[int] = set()
+    compy: Compy = Compy()
+    infile: Path = Path(__file__).resolve().parent / filename
+    compy.load_program(infile)
+    while True:
+        if compy.index in indices:
+            return compy.accumulator
+        else:
+            indices.add(compy.index)
+            compy.step()
 
 
-def part2(filename: str = "input.txt") -> int:
-    """Solve part 2 of the puzzle.
+# def part2(filename: str = "input.txt") -> int:
+#     """Solve part 2 of the puzzle.
 
-    Parameters
-    ----------
-    filename: str
-        The name of the file in this directory to load
+#     Parameters
+#     ----------
+#     filename: str
+#         The name of the file in this directory to load
 
-    Returns
-    -------
-    int:
-        The number of bags that the shiny gold bag contains
-    """
-    pass
+#     Returns
+#     -------
+#     int:
+#         The number of bags that the shiny gold bag contains
+#     """
+#     pass
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -5,22 +5,22 @@ from advent.day08 import main
 def test_part_1_example():
     """Check the example for part 1."""
     test_result = main.part1("example.txt")
-    assert test_result == 4
+    assert test_result == 5
 
 
 def test_part_1_actual():
     """Check the example for part 1."""
     test_result = main.part1()
-    assert test_result == 121
+    assert test_result == 1548
 
 
-def test_part_2_example():
-    """Check the example for part 2."""
-    test_result = main.part2("example.txt")
-    assert test_result == 32
+# def test_part_2_example():
+#     """Check the example for part 2."""
+#     test_result = main.part2("example.txt")
+#     assert test_result == 32
 
 
-def test_part_2_actual():
-    """Check the example for part 2."""
-    test_result = main.part2()
-    assert test_result == 3805
+# def test_part_2_actual():
+#     """Check the example for part 2."""
+#     test_result = main.part2()
+#     assert test_result == 3805
