13daa2ca6c935f7068e9f46bad292e3107f2e281
[Ian Preston] <ian.preston@wcb.ab.ca>
2020-12-08 09:28:06 -0700

    finished part 2

--- src/advent/day08/main.py
+++ src/advent/day08/main.py
@@ -20,6 +20,7 @@ class Compy:
         self.index: int = 0
         self.accumulator: int = 0
         self.instructions: List[Instruction] = []
+        self.run_complete: bool = False
 
     @staticmethod
     def _parse_instruction(line: str) -> Instruction:
@@ -50,6 +51,7 @@ class Compy:
             self.instructions = [
                 self._parse_instruction(line) for line in f.readlines()
             ]
+        self.run_complete = False
 
     def _exec_instruction(self, instruction: Instruction) -> None:
         """Run an instruction.
@@ -66,17 +68,24 @@ class Compy:
             self.accumulator += instruction.num
             self.index += 1
         elif instruction.cmd == "jmp":
-            if 0 <= self.index + instruction.num < len(self.instructions):
+            if 0 <= self.index + instruction.num <= len(self.instructions):
                 self.index += instruction.num
             else:
                 raise IndexError("Jumped out of instruction set")
         else:
             raise ValueError(f"Unsupported instruction: {instruction.cmd}")
+        if self.index == len(self.instructions):
+            self.run_complete = True
 
     def step(self) -> None:
         """Execute the next instruction."""
         instruction: Instruction = self.instructions[self.index]
         self._exec_instruction(instruction)
+
+    def full_run(self) -> None:
+        """Execute the program all the way through."""
+        while not self.run_complete:
+            self.step()
 
 
 def part1(filename: str = "input.txt") -> int:
@@ -104,17 +113,72 @@ def part1(filename: str = "input.txt") -> int:
             compy.step()
 
 
-# def part2(filename: str = "input.txt") -> int:
-#     """Solve part 2 of the puzzle.
+def loops_forever(instruction_list: List[Instruction]) -> bool:
+    """Check if a program is an infinite loop.
 
-#     Parameters
-#     ----------
-#     filename: str
-#         The name of the file in this directory to load
+    Parameters
+    ----------
+    instruction_list: List[Instruction]
+        The instruction list to check
 
-#     Returns
-#     -------
-#     int:
-#         The number of bags that the shiny gold bag contains
-#     """
-#     pass
+    Returns
+    -------
+    bool:
+        Whether the program will loop forever or not
+    """
+    indices: Set[int] = set()
+    compy: Compy = Compy()
+    compy.instructions = instruction_list
+    while True:
+        if compy.index in indices:
+            return True
+        elif compy.run_complete:
+            return False
+        else:
+            indices.add(compy.index)
+            compy.step()
+
+
+def part2(filename: str = "input.txt") -> int:
+    """Solve part 2 of the puzzle.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file in this directory to load
+
+    Returns
+    -------
+    int:
+        The value of the accumulator after a complete run
+    """
+    # Has to be an index that's encountered
+    indices: Set[int] = set()
+    compy: Compy = Compy()
+    infile: Path = Path(__file__).resolve().parent / filename
+    compy.load_program(infile)
+    original_instructions: List[Instruction] = compy.instructions.copy()
+    while compy.index not in indices:
+        indices.add(compy.index)
+        compy.step()
+    # Bad instruction isn't necessarily the last one before the loop
+    # I think I have to test all of them
+    for candidate_index in indices:
+        candidate_instruction: Instruction = compy.instructions[candidate_index]
+        # If it's an acc step we know not to change it
+        if candidate_instruction.cmd not in ("jmp", "nop"):
+            continue
+        swap_cmd: str
+        if candidate_instruction.cmd == "jmp":
+            swap_cmd = "nop"
+        else:
+            swap_cmd = "jmp"
+        swap_instruction: Instruction = Instruction(swap_cmd, candidate_instruction.num)
+        swapped_instructions: List[Instruction] = original_instructions.copy()
+        swapped_instructions[candidate_index] = swap_instruction
+        if not loops_forever(swapped_instructions):
+            break
+    compy = Compy()
+    compy.instructions = swapped_instructions
+    compy.full_run()
+    return compy.accumulator
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -14,13 +14,13 @@ def test_part_1_actual():
     assert test_result == 1548
 
 
-# def test_part_2_example():
-#     """Check the example for part 2."""
-#     test_result = main.part2("example.txt")
-#     assert test_result == 32
+def test_part_2_example():
+    """Check the example for part 2."""
+    test_result = main.part2("example.txt")
+    assert test_result == 8
 
 
-# def test_part_2_actual():
-#     """Check the example for part 2."""
-#     test_result = main.part2()
-#     assert test_result == 3805
+def test_part_2_actual():
+    """Check the example for part 2."""
+    test_result = main.part2()
+    assert test_result == 1375
