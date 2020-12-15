84ed9e3a7a19fcb4958a2ad12dcc748d2bb0061d
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-12-15 20:44:50 +0000

    finished part 2

--- src/advent/day15/main.py
+++ src/advent/day15/main.py
@@ -5,13 +5,17 @@ from collections import defaultdict
 from typing import Dict, List, Tuple
 
 
-def part1(start_seq: Tuple[int, ...] = (9, 19, 1, 6, 0, 5, 4)) -> int:
+def part1(
+    start_seq: Tuple[int, ...] = (9, 19, 1, 6, 0, 5, 4), stop_point: int = 2020
+) -> int:
     """Solve part 1 of the puzzle.
 
     Parameters
     ----------
-    start_seq: List[int]
+    start_seq: Tuple[int]
         The numbers to start the memory game
+    stop_point: int
+        How far along to play the memory game.
 
     Returns
     -------
@@ -26,11 +30,29 @@ def part1(start_seq: Tuple[int, ...] = (9, 19, 1, 6, 0, 5, 4)) -> int:
         turn_counter[num].append(turn)
         turn += 1
     last_num: int = start_seq[-1]
-    while turn <= 2020:
+    while turn <= stop_point:
         if len(turn_counter[last_num]) < 2:
             last_num = 0
         else:
+            # keep memory from exploding maybe?
+            turn_counter[last_num] = turn_counter[last_num][-2:]
             last_num = turn_counter[last_num][-1] - turn_counter[last_num][-2]
         turn_counter[last_num].append(turn)
         turn += 1
     return last_num
+
+
+def part2(start_seq: Tuple[int, ...] = (9, 19, 1, 6, 0, 5, 4)) -> int:
+    """Solve part 2 of the puzzle.
+
+    Parameters
+    ----------
+    start_seq: Tuple[int]
+        The numbers to start the memory game
+
+    Returns
+    -------
+    int:
+        The answer to part 2
+    """
+    return part1(start_seq, 30_000_000)
