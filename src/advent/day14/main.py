f7b7dfc0ecb1c16f89fef1da1a08c281d775f10f
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-12-15 01:51:18 +0000

    finished part 1

--- src/advent/day14/main.py
+++ src/advent/day14/main.py
@@ -1,12 +1,128 @@
 """Day 14 of the advent of code challenge."""
 from __future__ import annotations
 
-import itertools
+from collections import Counter
 from pathlib import Path
-from typing import Dict, Iterable, List, NamedTuple, Tuple
+import re
+from typing import Generator, List, NamedTuple, Optional
 
 
-def read_inputs(filename: str = "input.txt") -> List[Tuple[str, int]]:
+def int_to_bits(num: int) -> str:
+    """Turn an integer into a 32 bit unsigned int.
+
+    Parameters
+    ----------
+    num: int
+        input integer
+    
+    Returns
+    -------
+    str
+        32 bit unsigned integer
+    """
+    return f"{num:036b}"
+
+
+def bits_to_int(bits: str) -> int:
+    """Turn a bitstring back to an integer.
+    
+    Parameters
+    ----------
+    bits: str
+        String representation of binary
+    
+    Returns
+    -------
+    int
+        The integer equivalent
+    """
+    return int(bits, 2)
+
+
+class Bitmask(NamedTuple):
+    """Replace a bit at a point."""
+
+    index: int
+    bit: str
+
+
+class Address(NamedTuple):
+    """Index and value."""
+
+    index: int
+    value: int
+
+
+class FileLine(NamedTuple):
+    """Line from the file, can be a bitmask or an address."""
+
+    bitmasks: Optional(List[Bitmask])
+    address: Optional(Address)
+
+
+def parse_bitmasks(line: str) -> List[Bitmask]:
+    """Turn a line of text into a bitmask.
+
+    Parameters
+    ----------
+    line: str
+        raw input line for a bitmask
+    
+    Returns
+    -------
+    Bitmask:
+        parsed bitmask
+    """
+    return [
+        Bitmask(i, v)
+        for i, v in enumerate(line.strip().replace("mask = ", ""))
+        if v != "X"
+    ]
+
+
+def apply_bitmasks(bitmasks: List[Bitmask], value: int) -> int:
+    """Apply a bitmask to an integer.
+
+    Parameters
+    ----------
+    bitmasks: List[Bitmask]
+        List of bitmasks to apply
+    value: int
+        The integer to apply the bitmask to
+    
+    Returns
+    -------
+    int
+        Integer value after bitmask is applied
+    """
+    value_bitstring = int_to_bits(value)
+    value_bitlist = [c for c in value_bitstring]
+    for bitmask in bitmasks:
+        value_bitlist[bitmask.index] = bitmask.bit
+    return bits_to_int("".join(c for c in value_bitlist))
+
+
+def parse_address(line: str) -> Address:
+    """Line of text to address to update.
+    
+    Parameters
+    ----------
+    line: str
+        Turn a line from the input into an Address
+    
+    Returns
+    -------
+    Address
+        Parsed index and value
+    """
+    rgx = r"mem\[(\d+)\] = (\d+)"
+    address_str: str
+    value_str: str
+    address_str, value_str = re.match(rgx, line).groups()
+    return Address(int(address_str), int(value_str))
+
+
+def read_inputs(filename: str = "input.txt") -> Generator(FileLine):
     """Read in and parse a text file of inputs.
 
     Parameters
@@ -14,14 +130,21 @@ def read_inputs(filename: str = "input.txt") -> List[Tuple[str, int]]:
     filename: str
         The name of the file in this directory to load
 
-    Returns
-    -------
-    list[Tuple[str, int]]
-        The directions
+    Yields
+    ------
+    FileLine
+        parsed line from the file
     """
     in_path: Path = Path(__file__).resolve().parent / filename
     with open(in_path, "r") as f:
-        return [(line[0], int(line[1:])) for line in f.readlines()]
+        for line in f.readlines():
+            if line.startswith("mask"):
+                yield FileLine(parse_bitmasks(line), None)
+            elif line.startswith("mem"):
+                yield FileLine(None, parse_address(line))
+            else:
+                raise ValueError(f"unrecognized line: {line}")
+
 
 def part1(filename: str = "input.txt") -> int:
     """Solve part 1 of the puzzle.
@@ -36,11 +159,17 @@ def part1(filename: str = "input.txt") -> int:
     int:
         The answer to part 1
     """
-    directions: List[Tuple[str, int]] = read_inputs(filename)
-    boaty_mc_boatface = Ship()
-    for direction in directions:
-        boaty_mc_boatface.take_direction_deprecated(direction)
-    return boaty_mc_boatface.manhattan_dist
+    # initialize empty bitmask
+    bitmasks: List[Bitmask] = list()
+    addresses: Counter = Counter()
+    for fileline in read_inputs(filename):
+        if fileline.bitmasks is not None:
+            bitmasks = fileline.bitmasks
+        elif fileline.address is not None:
+            addresses[fileline.address.index] = apply_bitmasks(
+                bitmasks, fileline.address.value
+            )
+    return sum(addresses.values())
 
 
 def part2(filename: str = "input.txt") -> int:
@@ -56,8 +185,4 @@ def part2(filename: str = "input.txt") -> int:
     int:
         The answer to part 2
     """
-    directions: List[Tuple[str, int]] = read_inputs(filename)
-    boaty_mc_boatface = Ship()
-    for direction in directions:
-        boaty_mc_boatface.take_direction_updated(direction)
-    return boaty_mc_boatface.manhattan_dist
+    return 1
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -5,13 +5,13 @@ from advent.day14 import main
 def test_part_1_example1():
     """Check the example for part 1."""
     test_result = main.part1("example.txt")
-    assert test_result == 295
+    assert test_result == 165
 
 
 def test_part_1_actual():
     """Check the example for part 1."""
     test_result = main.part1()
-    assert test_result == 6559
+    assert test_result == 13105044880745
 
 
 # def test_part_2_example1():
