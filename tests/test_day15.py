c1f0e67a7f4b19c6a93b164233e96e7f7a240ab2
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-12-15 20:43:44 +0000

    comment out so future days don't take forever

--- tests/test_day15.py
+++ tests/test_day15.py
@@ -14,13 +14,13 @@ def test_part_1_actual():
     assert test_result == 1522
 
 
-def test_part_2_example1():
-    """Check the example for part 1."""
-    test_result = main.part2([0, 3, 6])
-    assert test_result == 175594
+# def test_part_2_example1():
+#     """Check the example for part 1."""
+#     test_result = main.part2([0, 3, 6])
+#     assert test_result == 175594
 
 
-def test_part_2_actual():
-    """Check the example for part 1."""
-    test_result = main.part2([9, 19, 1, 6, 0, 5, 4])
-    assert test_result == 18234
+# def test_part_2_actual():
+#     """Check the example for part 1."""
+#     test_result = main.part2([9, 19, 1, 6, 0, 5, 4])
+#     assert test_result == 18234
